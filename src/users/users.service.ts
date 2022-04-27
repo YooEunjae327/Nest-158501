4371ebf31281d50319a3bee869b38b5185bf449b
aaaaaa <91647398+YooEunjae327@users.noreply.github.com>
2022-04-27 21:11:56 +0900

    .

---
+++ src/users/users.service.ts
@@ -0,0 +1,9 @@
+import { Injectable } from '@nestjs/common';
+import * as uuid from 'uuid';
+
+@Injectable()
+export class UsersService {
+  async createUser(name: string, email: string, password: string) {
+    await this.ch;
+  }
+}
